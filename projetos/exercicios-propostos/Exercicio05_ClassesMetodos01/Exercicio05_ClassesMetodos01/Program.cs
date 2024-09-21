9bccc4f922de54abee0d0784e25aeb4b1405f7a3
Luis Miguel <luismiguelsantossouza@gmail.com>
2024-09-21 18:25:17 -0300

    add Exercicio05 classes, metodos e objetos exercicio01

---
+++ projetos/exercicios-propostos/Exercicio05_ClassesMetodos01/Exercicio05_ClassesMetodos01/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+using Exercicio05_ClassesMetodos01;
+
+Retangulo retangulo = new Retangulo();
+
+Console.WriteLine("Entre com a largura e altura do retângulo: ");
+retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine(retangulo);
---
+++ projetos/exercicios-propostos/Exercicio05_ClassesMetodos01/Exercicio05_ClassesMetodos01/Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Exercicio05_ClassesMetodos01;
+
+public class Retangulo
+{
+    public double Largura;
+    public double Altura;
+
+    public double Area()
+    {
+        return Largura * Altura;
+    }
+
+    public double Perimetro()
+    {
+        return (Largura + Altura) * 2;
+    }
+    
+    public double Diagonal()
+    {
+        return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+    }
+
+    public override string ToString()
+    {
+        return "Area: " + Area().ToString("F2", CultureInfo.InvariantCulture) 
+              + " Diagonal: "
+              + Diagonal().ToString("F2", CultureInfo.InvariantCulture)
+              + " Perimetro: "
+              +  Perimetro().ToString("F2", CultureInfo.InvariantCulture);
+    }
+}

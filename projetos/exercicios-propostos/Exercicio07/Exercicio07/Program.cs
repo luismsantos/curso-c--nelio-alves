d777792bc3141e7df520701e4697b391ed287f83
Luis Miguel <luismiguelsantossouza@gmail.com>
2024-09-21 19:15:57 -0300

    add Exercicio07 classes, metodos e objetos exercicio03

---
+++ projetos/exercicios-propostos/Exercicio07/Exercicio07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using Exercicio07;
+
+Aluno aluno = new Aluno();
+
+Console.WriteLine("Digite o nome do aluno: ");
+aluno.Nome = Console.ReadLine();
+
+Console.WriteLine("Digite as três notas do aluno: ");
+aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+if (aluno.Aprovado())
+{
+    Console.WriteLine("APROVADO");
+}
+else
+{
+    Console.WriteLine("REPROVADO");
+    Console.WriteLine("FALTARAM " + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+}

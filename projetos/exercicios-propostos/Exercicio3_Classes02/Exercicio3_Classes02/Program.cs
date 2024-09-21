c7a8030791b4e8c38be74ee62c7f0770f6738748
Luis Miguel <luismiguelsantossouza@gmail.com>
2024-09-21 16:53:42 -0300

    add exercicio2 de classes

---
+++ projetos/exercicios-propostos/Exercicio3_Classes02/Exercicio3_Classes02/Program.cs
@@ -0,0 +1,19 @@
+﻿using Exercicio3_Classes02;
+
+Funcionario funcionario = new Funcionario();
+
+var funcionario1 = funcionario;
+var funcionario2 = funcionario;
+
+Console.WriteLine("Digite o nome do primeiro funcionário: ");
+funcionario1.Nome = Console.ReadLine();
+Console.WriteLine("Digite o salário do primeiro funcionário: ");
+funcionario1.Salario = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o nome do segundo funcionário: ");
+funcionario2.Nome = Console.ReadLine();
+Console.WriteLine("Digite o salário do segundo funcionario: ");
+funcionario2.Salario = double.Parse(Console.ReadLine());
+
+double media = (funcionario1.Salario + funcionario2.Salario) / 2;
+Console.WriteLine("Salário médio: " + media);

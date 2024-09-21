dda08befde4a8e8aecd27eb4e77fcb7ad6b8c49c
Luis Miguel <luismiguelsantossouza@gmail.com>
2024-09-21 18:08:08 -0300

    add exercicio4 problema de classes feito

---
+++ projetos/exercicios-propostos/Exercicio4_ProblemaClasses01/Exercicio4_ProblemaClasses01/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Exercicio4_ProblemaClasses01;
+
+public class Produto
+{
+    public string Nome;
+    public double Preco;
+    public int Quantidade;
+
+    public double ValorTotalEmEstoque()
+    {
+        return Preco * Quantidade;
+    }
+
+    public void AdicionarProduto(int quantidade)
+    {
+        Quantidade += quantidade;
+    }
+
+    public void RemoverProduto(int quantidade)
+    {
+        Quantidade -= quantidade;
+    }
+
+    public override string ToString()
+    {
+        return Nome
+               + ", $ "
+               + Preco.ToString("F2", CultureInfo.InvariantCulture)
+               + ", "
+               + Quantidade
+               + " unidades, Total: $ "
+               + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+    }
+}
---
+++ projetos/exercicios-propostos/Exercicio4_ProblemaClasses01/Exercicio4_ProblemaClasses01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Channels;
+using Exercicio4_ProblemaClasses01;
+
+Produto produto = new Produto();
+
+Console.WriteLine("Digite o nome do produto: ");
+produto.Nome = Console.ReadLine();
+Console.WriteLine("Digite o preço do produto: ");
+produto.Preco = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a quantidade no estoque: ");
+produto.Quantidade = int.Parse(Console.ReadLine());
+Console.WriteLine("Dados do produto: " + produto);
+
+Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+produto.AdicionarProduto(int.Parse(Console.ReadLine()));
+Console.WriteLine("Dados do produto: " + produto);
+
+Console.Write("Digite o número de produtos a ser removido do estoque: ");
+produto.RemoverProduto(int.Parse(Console.ReadLine()));
+Console.WriteLine("Dados do produto: " + produto);
+
+
+
+

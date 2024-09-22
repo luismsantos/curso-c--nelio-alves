13f223895f5dfddaa4e1ca4a164c0184f2ab41d9
Luis Miguel <luismiguelsantossouza@gmail.com>
2024-09-22 18:33:06 -0300

    add exercicio de fixação encapsulamento

---
+++ projetos/exercicios-propostos/Exercicio09_Encapsulamento/Exercicio09_Encapsulamento/Banco.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Exercicio09_Encapsulamento;
+
+public class Banco
+{
+    public string Nome { get; set; }
+    public int Conta { get; }
+    public double Saldo { get; private set; } = 0;
+
+    public Banco(int conta, string nome)
+    {
+        Nome = nome;
+        Conta = conta;
+    }
+
+    public void Depositar(double valor)
+    {
+        Saldo += valor;
+    }
+
+    public void Sacar(double valor) //cobra 5 reais de taxa
+    {
+        Saldo -= valor + 5.0;
+    }
+
+    public override string ToString()
+    {
+        return "Dados da conta: " + "\nConta: " +  Conta + " \nTitular: " + Nome + " \nSaldo: R$"+ Saldo.ToString("F2", CultureInfo.InvariantCulture);
+    }
+}
+
---
+++ projetos/exercicios-propostos/Exercicio09_Encapsulamento/Exercicio09_Encapsulamento/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+using Exercicio09_Encapsulamento;
+
+Console.Write("Entre o número da conta: ");
+int conta = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Entre o titular da conta: ");
+string titular = Console.ReadLine();
+
+Console.WriteLine("Haverá deposito inicial? (S/N)");
+char c = char.Parse(Console.ReadLine().ToUpper());
+
+Banco banco = new Banco(conta,titular);
+
+double deposito;
+double saque;
+
+if (c == 'S')
+{
+    Console.WriteLine("Entre o valor do deposito inicial: ");
+    deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    
+    banco.Depositar(deposito);
+    Console.WriteLine(banco);
+
+    Console.WriteLine("Entre um valor de depósito: ");
+    deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    
+    banco.Depositar(deposito);
+    Console.WriteLine(banco);
+
+    Console.WriteLine("Entre um valor de saque: ");
+    saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    
+    banco.Sacar(saque);
+    Console.WriteLine(banco);
+}
+
+if (c == 'N')
+{
+    Console.WriteLine(banco);
+
+    Console.WriteLine("Entre um valor de depósito: ");
+    deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    banco.Depositar(deposito);
+    Console.WriteLine(banco);
+
+    Console.WriteLine("Entre um valor de saque: ");
+    saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    banco.Sacar(saque);
+    Console.WriteLine(banco);
+}
+
+
+
+
+

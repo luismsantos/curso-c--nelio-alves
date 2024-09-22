76d3b4f6fd44a2d202fb2e1ed2969166e2c7860d
Luis Miguel <luismiguelsantossouza@gmail.com>
2024-09-21 23:34:02 -0300

    add exercicio de fixação de membros estaticos

---
+++ projetos/exercicios-propostos/Exercicio08_MembrosEstaticos/Exercicio08_MembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio08_MembrosEstaticos;
+
+public class ConversorDeMoeda
+{
+
+    public static double IOF = 0.06;
+    
+    public static double ConverterValor(double dolar, double quantidade)
+    {
+        double total = quantidade * dolar;
+        return total + total * IOF;
+    }
+}
---
+++ projetos/exercicios-propostos/Exercicio08_MembrosEstaticos/Exercicio08_MembrosEstaticos/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+using Exercicio08_MembrosEstaticos;
+
+Console.WriteLine("Qual é a cotação do dolar? ");
+double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Quantos dolares você vai comprar? ");
+double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Valor a ser pago em reais = " + ConversorDeMoeda.ConverterValor(dolar, quantidade).ToString("F2", CultureInfo.InvariantCulture));
+

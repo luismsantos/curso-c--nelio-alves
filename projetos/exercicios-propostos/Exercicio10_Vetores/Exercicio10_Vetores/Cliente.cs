495457de1588ba22629a117a19fc28906e436fce
Luis Miguel <luismiguelsantossouza@gmail.com>
2024-09-23 17:47:28 -0300

    terminar

---
+++ projetos/exercicios-propostos/Exercicio10_Vetores/Exercicio10_Vetores/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicio10_Vetores;
+
+public class Cliente
+{
+    public string Nome;
+    public string Email;
+    public int Quarto;
+    
+    
+    
+}
---
+++ projetos/exercicios-propostos/Exercicio10_Vetores/Exercicio10_Vetores/Program.cs
@@ -0,0 +1,10 @@
+﻿using Exercicio10_Vetores;
+
+Console.WriteLine("Quanto quartos vão ser alugados? ");
+int nQuartos = int.Parse(Console.ReadLine());
+
+int[] quartos = new int[10];
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(quartos[i]);
+}

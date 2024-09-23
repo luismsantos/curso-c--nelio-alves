e6441345e2ee227eafe9409a427744356ff9101a
Luis Miguel <luismiguelsantossouza@gmail.com>
2024-09-23 17:34:39 -0300

    add aula02 vetores

---
+++ projetos/Vetores/Vetores/Produto.cs
@@ -0,0 +1,7 @@
+﻿namespace DefaultNamespace;
+
+public class Produto
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+}
--- projetos/Vetores/Vetores/Program.cs
+++ projetos/Vetores/Vetores/Program.cs
@@ -1,4 +1,6 @@
-﻿// Faça um programa para ler um número inteiro N e a altura de N pessoas. Armazene as N alturas em um vetor
+﻿using System;
+// Exercicio 1
+// Faça um programa para ler um número inteiro N e a altura de N pessoas. Armazene as N alturas em um vetor
 // Em seguida, mostrar a altura médicas dessas pessoas.
 
 using System.Globalization;
@@ -21,3 +23,28 @@ for (int i = 0; i < n; i++)
 
 double media = vetor.Average();
 Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+
+
+// Exercicio 2 - Exemplo 
+
+int n = int.Parse(Console.ReadLine());
+
+Produto[] vetor = new Produto[n];
+
+for (int i = 0; i < n; i++)
+{
+    string nome = Console.ReadLine();
+    double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    vetor[i] = new Produto(Nome = nome, Preco = preco);
+}
+
+double soma = 0;
+for (int i = 0; i < n; i++)
+{
+    sum+= vetor[i].Preco;
+}
+
+System.Console.WriteLine(vetor.Average);
+    
+    
+

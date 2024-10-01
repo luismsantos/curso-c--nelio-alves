91101eeeadcb2314cfb8fb313dfc71171fdbb28c
luis <luismiguelsantossouza@gmail.com>
2024-10-01 11:18:19 -0300

    add exercicio11 sobre matrizes | 01

---
+++ projetos/exercicios-propostos/Exercicio11_Matriz01/Exercicio11_Matriz01/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Digite o número de linhas e colunas: ");
+int a = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[a, a];
+
+for (int i = 0; i < a; i++) // responsavel por percorrer linhas
+{
+    string[] valores = Console.ReadLine().Split(' ');
+    
+    for (int j = 0; j < a; j++) // responsavel por percorrer colunas
+    {
+        matriz[i, j] = int.Parse(valores[j]);
+    }
+}
+
+Console.WriteLine("Diagonal principal: ");
+for (int i = 0; i < a; i++)
+{
+    Console.Write(matriz[i,i] + " ");
+}
+
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < a; i++) // estrutura básica para percorrer uma matriz inteira
+{
+    for (int j = 0; j < a; j++)
+    {
+        if (matriz[i,j] < 0)
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine("Numeros negativos: " + count);
